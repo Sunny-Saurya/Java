b5ecee21ee98aba245b42d2728808664128aa93a
Sunny Kumar <sunnysaurya9955@gmail.com>
2024-12-25 22:45:13 +0530

    hollow ractangle

---
+++ ques5.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class ques5 {
+    
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows :");
+        int r = sc.nextInt();
+
+        System.out.print("Enter the number of rows :");
+        int c = sc.nextInt();
+
+        for(int i = 1; i <= r; i++)
+        {
+            for(int j = 1; j <= c; j++)
+            {
+                if(i == 1 || j == 1 || i == r || j == c)
+                {
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+
+        
+    }
+}
