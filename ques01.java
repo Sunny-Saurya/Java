358a49b1e1a2bc0892adccfdbdf13dc5bd649d45
Sunny Kumar <sunnysaurya9955@gmail.com>
2024-12-25 21:50:35 +0530

    simple program

---
+++ ques01.java
@@ -0,0 +1,18 @@
+public class ques01 {
+    
+    public static void main(String[] args) 
+    {
+
+        int a = 25;
+        int b = 12;
+        int sum = a + b;
+        int mul = a * b;
+        int div = a / b;
+        int mod = a % b;
+
+        System.out.println(sum);
+        System.out.println(mul);
+        System.out.println(div);
+        System.out.println(mod);
+    }
+}
