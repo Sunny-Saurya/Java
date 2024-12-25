3cb961dde8b5cc54633feff4cce3d91f1fc2ded2
Sunny Kumar <sunnysaurya9955@gmail.com>
2024-12-25 21:57:23 +0530

    input/output in java

---
+++ basic.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class basic {
+    public static void main(String[] args) 
+    {
+        // Input
+        Scanner  sc = new Scanner(System.in);
+        
+        // for taking input from user as integer
+
+        int n = sc.nextInt();
+        System.out.println(n);
+
+        // for taking input from user as string
+        String s = sc.next();
+        System.out.println(s);
+
+        // for taking input from user as string in one line
+        String s1 = sc.nextLine();
+        System.out.println(s1);
+        
+
+        
+    }
+}
