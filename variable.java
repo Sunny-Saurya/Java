5c0959fda2fc9401745bf586ee2030d48535c214
Sunny Kumar <sunnysaurya9955@gmail.com>
2024-12-25 21:34:34 +0530

    variables in java

---
+++ variable.java
@@ -0,0 +1,10 @@
+public class variable {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello world");
+        System.out.println("Hello world with Java");
+        
+    }
+}
+
+// ln is use for printing output to the next line
