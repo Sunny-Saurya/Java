b5a7ee22529b787c65349e965e51579ab2f1cb64
Sunny Kumar <sunnysaurya9955@gmail.com>
2026-06-18 20:29:21 +0530

    Loops in Java

---
+++ Revision/oracleJdk-26/Loops.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Loops{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // for(int i = 0; i < n; i++){
+        //     System.out.println(i);
+        // }
+
+        int i = 0;
+
+        while(i < n){
+            System.out.println(i);
+            i++;
+        }
+
+
+    }
+}
