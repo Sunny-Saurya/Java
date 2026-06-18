39c2b649797a8800af17a0238cb92a3a0e01295c
Sunny Kumar <sunnysaurya9955@gmail.com>
2026-06-18 22:34:56 +0530

    Functions in Java

---
+++ Revision/oracleJdk-26/FunctionsDemo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class FunctionsDemo{
+
+    static int add(int a, int b){
+        return a + b;
+    }
+
+    public static void main(String[] args){
+        System.out.println("Addition : " + add(3,4));
+    }
+}
