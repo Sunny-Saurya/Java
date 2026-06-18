398c16da1d0c1b69f439da3d9c2f892c774fc705
Sunny Kumar <sunnysaurya9955@gmail.com>
2026-06-18 20:20:24 +0530

    Input file

---
+++ Revision/oracleJdk-26/Input.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Input{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        String str = sc.next();
+        int ans = a + b;
+        System.out.println(ans);
+        System.out.println(str);
+    }
+}
